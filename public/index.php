736aa8d0af85a58a259950c6a44f9c68937c964f
Sphinx06 <Vous@exemple.com>
2018-10-17 17:12:30 +0200

    Page d'accueil | CSS | ASSETS ajoutés

--- public/index.php
+++ public/index.php
@@ -10,34 +10,93 @@ $connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=
 $userRepository = new \User\UserRepository($connection);
 $users = $userRepository->fetchAll();
 ?>
+<!DOCTYPE html>
+<html>  
 
-<html>
-<head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
-<body>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-<div class="container">
-    <h3><?php echo 'Hello world from Docker! php' . PHP_VERSION; ?></h3>
+        <title>TEST ACCUEIL</title>
 
-    <table class="table table-bordered table-hover table-striped">
-        <thead style="font-weight: bold">
-            <td>#</td>
-            <td>Firstname</td>
-            <td>Lastname</td>
-            <td>Age</td>
-        </thead>
-        <?php /** @var \User\User $user */
-        foreach ($users as $user) : ?>
-            <tr>
-                <td><?php echo $user->getId() ?></td>
-                <td><?php echo $user->getFirstname() ?></td>
-                <td><?php echo $user->getLastname() ?></td>
-                <td><?php echo $user->getAge() ?> years</td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
-</div>
-</body>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!-- Font Awesome JS -->
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/bard_main.css">
+    </head>
+
+    <body>
+
+        <!-- NAVBAR !-->
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top" id="banner">
+            <div class="container">
+                <!-- Brand -->
+                <a class="navbar-brand" href="#"><img id="img-navbar" src="assets/logo_sf.png" style="width:75px;">Bar <span>D</span></a>
+
+                <!-- BANNIERE ACCUEIL !-->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Catalogue</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Se connecter</a>
+                        </li> 
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- BANNIERE ACCUEIL !-->
+        <div class="banner">
+            <div class="container">
+                <div class="banner-text">
+                    <div class="banner-heading">
+                        Le bar digital vous ouvre ses portes
+                    </div>
+                    <div class="banner-sub-heading">
+                        ENSIIE - EVRY
+                    </div>
+                    <button type="button" class="btn btn-warning text-dark btn-banner">Connecte toi</button>
+                </div>
+            </div>
+        </div>
+
+
+        <section id="about">
+        <div class="container">
+            <div class="text-intro">
+            <h2>Nous contacter</h2>
+                <p>Viens donc nous voir ! </p>
+            </div>
+        </div>
+        </section>
+
+
+        <!-- JAVASCRIPT -->
+        <script>
+            $(document).on("scroll", function(){
+                if($(document).scrollTop() > 86){
+                    $("#banner").addClass("shrink");
+                    $("#img-navbar").attr("src","assets/logo_sf_blanc.png");
+                }
+                else{
+                    $("#banner").removeClass("shrink");
+                    $("#img-navbar").attr("src","assets/logo_sf.png");
+                }
+            });
+        </script>
+    </body>
+
 </html>
