7ac1a9f40fa05d8a4c4d6a28cccc4764f19c71da
Toast <theo.peucker@gmail.com>
2018-10-21 00:30:44 +0200

    tentative vaine d'ajouter des tests

---
+++ test/unit/Product/ProductTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Product;
+
+use PHPUnit\Framework\TestCase;
+//use Product\Entity\Product;
+
+class ProductTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function test()
+    {
+        $product = new Product();
+        $product->setQuantity(4);
+        self::assertSame(4, $product->getQuantity());
+    }
+}
