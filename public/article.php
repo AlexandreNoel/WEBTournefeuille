867989709c5b0406fcb7007ca5ff22ce60e1446d
xavier.grimaldi@live.fr <Vous@exemple.com>
2018-10-21 19:48:43 +0200

    Amélioration responsive collapse navbar

--- public/article.php
+++ public/article.php
@@ -7,8 +7,8 @@ $dbUser = getenv('DB_USER');
 $dbPassword = getenv('DB_PASSWORD');
 $connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
 
-$userRepository = new \User\UserRepository($connection);
-$users = $userRepository->fetchAll();
+$username = "Sphinx06";
+$userFullName = "Xavier GRIMALDI";
 ?>
 
 <!DOCTYPE html>
--- public/stats.php
+++ public/stats.php
@@ -8,8 +8,6 @@
     $dbPassword = getenv('DB_PASSWORD');
     $connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
 
-    $userRepository = new \User\UserRepository($connection);
-    $users = $userRepository->fetchAll();
     $username = "Sphinx06";
     $userFullName = "Xavier GRIMALDI";
 ?>
