dd7da8e6731d173bdea146fb3beb6b0cb946f4a4
Olivier <olivier.marie-louise@ensiie.fr>
2018-10-23 17:56:58 +0200

    add restaurant entity, hydrator, repository

--- src/Hydrator/Restaurant.php
+++ src/Hydrator/Restaurant.php
@@ -5,5 +5,56 @@ namespace Hydrator;
 
 class Restaurant
 {
+    public function extract(\Entity\Restaurant $object): array
+    {
+        $data = [];
+        if ($object->getId()) {
+            $data['id_resto'] = $object->getId();
+        }
+
+        if ($object->getName()) {
+            $data['nom_resto'] = $object->getName();
+        }
+
+
+        if ($object->getDescription()) {
+            $data['descr_resto'] = $object->getDescription();
+        }
+
+        if ($object->getAddress()) {
+            $data['addr_resto'] = $object->getAddress();
+        }
+
+        if ($object->getZipCode()) {
+            $data['cp_resto'] = $object->getZipCode();
+        }
+
+        if ($object->getCity()) {
+            $data['city_resto'] = $object->getCity();
+        }
+
+        if ($object->getPhoneNumber()) {
+            $data['tel_resto'] = $object->getPhoneNumber();
+        }
+
+        if ($object->getUrl()) {
+            $data['website_resto'] = $object->getUrl();
+        }
+
+        return $data;
+    }
+
+    public function hydrate(array $data, \Entity\Restaurant $emptyEntity): \Entity\Restaurant
+    {
+        return $emptyEntity
+            ->setId($data['id_resto'] ?? null)
+            ->setName($data['nom_resto'] ?? null)
+            ->setDescription($data['descr_resto'] ?? null)
+            ->setAddress($data['addr_resto'] ?? null)
+            ->setZipCode($data['cp_resto'] ?? null)
+            ->setCity($data['city_resto'] ?? null)
+            ->setPhoneNumber($data['tel_resto'] ?? null)
+            ->setUrl($data['website_resto'] ?? null);
+    }
 
 }
