dfd8737c4b40dab5755d7afffc81ad7e608926bf
Olivier <olivier.marie-louise@ensiie.fr>
2018-10-26 00:59:44 +0200

    add link to restaurant website in description-restaurant

--- public/view/description-restaurant.php
+++ public/view/description-restaurant.php
@@ -13,31 +13,31 @@
         echo "not connected";
     } ?></h4>
 
-            <table class="table table-bordered table-hover table-striped">
-                <thead style="font-weight: bold">
-                <td>id</td>
-                <td>resto name</td>
-                <td>resto descr</td>
-                <td>resto addr</td>
-                <td>cpp addr</td>
-                <td>resto city</td>
-                <td>resto tel</td>
-                <td>resto website</td>
-                </thead>
+<table class="table table-bordered table-hover table-striped">
+    <thead style="font-weight: bold">
+    <td>id</td>
+    <td>resto name</td>
+    <td>resto descr</td>
+    <td>resto addr</td>
+    <td>cpp addr</td>
+    <td>resto city</td>
+    <td>resto tel</td>
+    <td>resto website</td>
+    </thead>
 
-                    <tr>
-                        <td><?php echo $resto->getId() ?></td>
-                        <td><?php echo $resto->getName() ?></td>
-                        <td><?php echo $resto->getDescription() ?></td>
-                        <td><?php echo $resto->getAddress() ?></td>
-                        <td><?php echo $resto->getZipCode() ?></td>
-                        <td><?php echo $resto->getCity() ?></td>
-                        <td><?php echo $resto->getPhoneNumber() ?></td>
-                        <td><?php echo $resto->getUrl() ?></td>
-                
-                
-                    </tr>
-            </table>
+    <tr>
+        <td><?php echo $resto->getId() ?></td>
+        <td><?php echo $resto->getName() ?></td>
+        <td><?php echo $resto->getDescription() ?></td>
+        <td><?php echo $resto->getAddress() ?></td>
+        <td><?php echo $resto->getZipCode() ?></td>
+        <td><?php echo $resto->getCity() ?></td>
+        <td><?php echo $resto->getPhoneNumber() ?></td>
+        <td><a target="_blank" href=<?php echo $resto->getUrl() ?>/><?php echo $resto->getUrl() ?></td>
+
+
+    </tr>
+</table>
 
 </body>
 </html>
