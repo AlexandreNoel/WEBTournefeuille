ceb4a12a9b34b263efc3ef97c9e5ba9eb85f8a75
Yoan <rockyoan@gmail.com>
2018-10-27 14:55:30 +0200

    fixup favorite

--- public/add-favorite-restaurant.php
+++ public/add-favorite-restaurant.php
@@ -8,24 +8,26 @@ session_start();
 $userRepository = new \Repository\User();
 $restoRepository = new \Repository\Restaurant();
 
+$error = [];
+
 if (isset($_SESSION['id'])) {
     $id_user = $_SESSION['id'];
     $id_restaurant = $_POST['id_resto'];
 
 if($restoRepository->isAlreadyFavorite($id_user, $id_restaurant)){
-    $restoRepository->deleteById($id_user, $id_restaurant);
-        $_SESSION['test'] = "deleted";
+    $restoRepository->deleteFavoriteById($id_user, $id_restaurant);
+    $error = "deleted";
 }else{
 
         if (!$restoRepository->addFavorite($id_user, $id_restaurant)) {
-            $_SESSION['test'] = "cant add in favorite";
+            $error = "cant add in favorite";
         }else{
-            $_SESSION['test'] = "added";
+            $error = "added";
         }
-    }
 
-} else{
-    $_SESSION['test'] = "not connected";
+} 
+}else{
+    $error = "not connected";
 }
 
 header('Location: index.php');
