b0d4013b37d23e8af4264eca24f120c564943c75
Axel Morvan <morvan.axel@gmail.com>
2018-10-27 19:13:31 +0200

    Add filters

--- public/js/restaurants.js
+++ public/js/restaurants.js
@@ -1,88 +1,139 @@
-let restaurants = [];
-let error = null;
+let filters = {
+    score: -1,
+    badge: '',
+    category: '',
+    favorite: false
+};
 
-getRestaurants((err, restaurants) => {
-    if (err) {
-        error = 'An error occured';
-    } else {
-        $(document).ready(() => {
-            const listRestos = $('#list-restaurants');
-            const templateResto = $('#rest-template');
+$(document).ready(() => {
+    getRestaurants();
 
-            listRestos.append(restaurants.map((restoData) => {
-                let restoDiv = templateResto.clone();
-                restoDiv.removeClass('hidden');
-
-
-                //restoDiv.find('#rest-link').attr('href', '/restaurants/' + restoData.id);
-                restoDiv.find('#rest-name').text(restoData.name);
-                restoDiv.find('#rest-cat').text(restoData.category);
-                restoDiv.find('#rest-thumb').attr('src', restoData.thumbnail);
-                if (restoData.favorite === true)
-                    restoDiv.find('#rest-favorite').removeClass('hidden');
-                else if (restoData.favorite === false)
-                    restoDiv.find('#rest-unfavorite').removeClass('hidden');
-
-                let stars = [];
-                for (let i = 0; i < 5; i++) {
-                    if (i < restoData.score)
-                        stars.push('<i class="fas fa-star"></i>');
-                    else
-                        stars.push('<i class="far fa-star"></i>');
-                }
-                restoDiv.find('#rest-score').replaceWith(stars);
-
-                let badges = restoData.badges.map((badge) => {
-                    let imageName = '';
-                    switch (badge) {
-                        case 'Bio':
-                            imageName = 'icons8-salade-de-laitue-64.png';
-                            break;
-                        case 'Vegan':
-                            imageName = 'icons8-marque-végétarienne-48.png';
-                            break;
-                        case 'Halal':
-                            imageName = 'icons8-signe-halal-50.png';
-                            break;
-                    }
-                    return `<img alt='${badge}'
-                    title='${badge}'
-                    src='/assets/images/icons_logo/${imageName}'/>`
-                });
-                restoDiv.find('#rest-badges').append(badges);
-                
-                return restoDiv;
-            }));
-        });
-    }
+    $('#score-filter').change(() => {
+        filters.score = parseInt($('#score-filter').val());
+        getRestaurants();
+    });
+    $('#badge-filter').change(() => {
+        filters.badge = $('#badge-filter').val();
+        getRestaurants();
+    });
+    $('#category-filter').change(() => {
+        filters.category = $('#category-filter').val();
+        getRestaurants();
+    });
+    $('#favorites-filter').change(() => {
+        filters.favorite = $('#favorites-filter').is(':checked');
+        getRestaurants();
+    });
 });
 
-function getRestaurants(callback) {
-    restaurants = new Array(5).fill({
-        name: 'Burger King',
-        description: 'Un bon gros phat bien connu',
-        category: 'Fast food',
-        address: '3 rue de la boustifaille',
-        postalCode: 91000,
-        city: 'Evry',
-        phone: '+331 23 45 67 89',
-        website: 'https://www.burgerking.fr/',
-        thumbnail: 'http://www.stickpng.com/assets/images/5842996fa6515b1e0ad75add.png',
-        favorite: true,
-        badges: ['Bio', 'Halal', 'Vegan'],
-    });
-
-    restaurants = restaurants.map((resto, index) => {
-        resto = JSON.parse(JSON.stringify(resto));
-        resto.id = index + 1;
-        resto.score = index + 1;
-        if (resto.id === 2 || resto.id === 5) {
-            resto.favorite = false;
-        } else if (resto.id ===3) {
-            resto.favorite = null;
+function getRestaurants() {
+    console.log(filters);
+    restaurants = [
+        {
+            id: 1,
+            score: 5,
+            name: 'Burger King',
+            description: 'Un bon gros phat bien connu',
+            category: 'Fast food',
+            address: '3 rue de la boustifaille',
+            postalCode: 91000,
+            city: 'Evry',
+            phone: '+331 23 45 67 89',
+            website: 'https://www.burgerking.fr/',
+            thumbnail: 'http://www.stickpng.com/assets/images/5842996fa6515b1e0ad75add.png',
+            favorite: false,
+            badges: [],
+        },
+        {
+            id: 2,
+            score: 2,
+            name: 'Mc Do',
+            description: 'Un peu moins bon',
+            category: 'Fast food',
+            address: '3 rue de la boustifaille',
+            postalCode: 91000,
+            city: 'Evry',
+            phone: '+331 23 45 67 89',
+            website: 'https://www.burgerking.fr/',
+            thumbnail: 'https://upload.wikimedia.org/wikipedia/fr/thumb/e/ea/Mcdonalds_France_2009_logo.svg/1138px-Mcdonalds_France_2009_logo.svg.png',
+            favorite: true,
+            badges: ['Bio', 'Vegan'],
+        },
+        {
+            id: 3,
+            score: 0,
+            name: 'Waffle',
+            description: 'Berk',
+            category: 'Chelou',
+            address: '3 rue de la boustifaille',
+            postalCode: 91000,
+            city: 'Evry',
+            phone: '+331 23 45 67 89',
+            website: 'https://www.burgerking.fr/',
+            thumbnail: 'https://content3.jdmagicbox.com/comp/delhi/s1/011pxx11.xx11.171205141322.i4s1/catalogue/the-waffle-factory-delhi-ht6if.jpg',
+            favorite: null,
+            badges: ['Bio', 'Halal', 'Vegan'],
         }
-        return resto;
-    });
+    ];
 
-    callback(null, restaurants);
+    buildContent(restaurants);
+}
+
+function buildContent(restaurants) {
+    const listRestos = $('#list-restaurants');
+    const templateResto = $('#rest-template');
+
+    listRestos.empty();
+    listRestos.append(restaurants.map((restoData) => {
+        let restoDiv = templateResto.clone();
+        restoDiv.removeClass('hidden');
+
+
+        restoDiv.find('#rest-link').attr('href', '/restaurants/' + restoData.id);
+        restoDiv.find('#rest-name').text(restoData.name);
+        restoDiv.find('#rest-cat').text(restoData.category);
+        restoDiv.find('#rest-thumb').attr('src', restoData.thumbnail);
+        if (restoData.favorite === true) {
+            restoDiv.find('#rest-favorite').removeClass('hidden');
+            restoDiv.find('#rest-unfavorite').addClass('hidden');
+        } else if (restoData.favorite === false) {
+            restoDiv.find('#rest-favorite').addClass('hidden');
+            restoDiv.find('#rest-unfavorite').removeClass('hidden');
+        } else {
+            restoDiv.find('#rest-favorite').addClass('hidden');
+            restoDiv.find('#rest-unfavorite').addClass('hidden');
+        }
+
+        let stars = [];
+        for (let i = 0; i < 5; i++) {
+            if (i < restoData.score)
+                stars.push('<i class="fas fa-star"></i>');
+            else
+                stars.push('<i class="far fa-star"></i>');
+        }
+        restoDiv.find('#rest-score').empty();
+        restoDiv.find('#rest-score').append(stars);
+
+        let badges = restoData.badges.map((badge) => {
+            let imageName = '';
+            switch (badge) {
+                case 'Bio':
+                    imageName = 'icons8-salade-de-laitue-64.png';
+                    break;
+                case 'Vegan':
+                    imageName = 'icons8-marque-végétarienne-48.png';
+                    break;
+                case 'Halal':
+                    imageName = 'icons8-signe-halal-50.png';
+                    break;
+            }
+            return `<img alt='${badge}'
+            title='${badge}'
+            src='/assets/images/icons_logo/${imageName}'/>`
+        });
+        restoDiv.find('#rest-badges').empty();
+        restoDiv.find('#rest-badges').append(badges);
+
+        return restoDiv;
+    }));
 }
