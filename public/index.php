aa9d771b7e95397988c9268916804beeb9c2fd3b
Axel Morvan <morvan.axel@gmail.com>
2018-10-27 11:14:16 +0200

    Adapt form to the new front struct

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
 $uri = $_SERVER['REQUEST_URI'];
-
-if (preg_match('/^\/burgers\/?$/', $uri)) {
+if (preg_match('/^\/form\/?$/', $uri)) {
+    $pagePath = 'form.html';
+} elseif (preg_match('/^\/burgers\/?$/', $uri)) {
     $pagePath = 'burgers.html';
 } elseif (preg_match('/^\/burgers\/[0-9]+\/?$/', $uri)) {
     $pagePath = 'burgers.html';
--- public/template.php
+++ public/template.php
@@ -2,9 +2,13 @@
     <head>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css">
         <link rel="stylesheet" href="/css/sidebar.css">
         <link rel="stylesheet" href="/css/phatadvisor.css">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css">
+        <link rel="stylesheet" href="/css/form.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="/js/sidebar.js"></script>
     </head>
     <body>
         <div id="header" class="row">
@@ -37,5 +41,3 @@
             </div>
     </body>
 </html>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="/js/sidebar.js"></script>
