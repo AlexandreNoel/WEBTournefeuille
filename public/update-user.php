d77cabf5002368a44019c3d110b9a82640016ee3
Olivier <olivier.marie-louise@ensiie.fr>
2018-10-28 20:24:53 +0100

    fix update-user

--- public/update-user.php
+++ public/update-user.php
@@ -16,49 +16,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
     $lastname = $post_vars['nom_user'] ?? null;
     $promo = $post_vars['promo_user'] ?? null;
     $mail = $post_vars['mail_user'] ?? null;
-    $password = $post_vars['secret_user'] ?? null;
-    $confirm_password = $post_vars['secret_user'] ?? null;
+    $oldPassword = $post_vars['secret_user_old'] ?? null;
+    $password = $post_vars['secret_user_new'] ?? null;
+    $confirm_password = $post_vars['secret_user_new2'] ?? null;
 
-    if ($id) {
+    $view = [
+        'user' => [
+            'id_user' => $id,
+            'prenom_user' => $firstname,
+            'nom_user' => $lastname ,
+            'promo_user' => $promo ,
+            'mail_user' => $mail ,
+            'old_password' => $oldPassword ,
+            'secret_user' => $password ,
+            'confirm_secret_user' => $confirm_password ,
+        ],
+        'errors',
+    ];
+
+    $userService = new \Service\User();
+    $view['errors'] = $userService->verify_update($userRepository, $view['user']);
+
+    if (count(array_filter($view['errors'])) === 0) {
+
         $user = $userRepository->findOneById($id);
 
-        if ($user) {
+        $user->setFirstname($firstname)
+            ->setLastname($lastname)
+            ->setMailAdress($mail)
+            ->setPromo($promo)
+            ->setPassword(password_hash($password, PASSWORD_BCRYPT));
 
-            $view = [
-                'user' => [
-                    'prenom_user' => $firstname,
-                    'nom_user' => $lastname ,
-                    'promo_user' => $promo ,
-                    'mail_user' => $mail ,
-                    'secret_user' => $password ,
-                    'confirm_secret_user' => $password ,
-                ],
-                'errors',
-            ];
-
-            $userService = new \Service\User();
-            $view['errors'] = $userService->verify_registration($userRepository, $view['user']);
-
-            //remove error mail_user_exist
-            unset($view['errors']['mail_user_exist']);
-
-            if (count(array_filter($view['errors'])) === 0) {
-                $user->setFirstname($firstname)
-                    ->setLastname($lastname)
-                    ->setMailAdress($mail)
-                    ->setPromo($promo)
-                    ->setPassword(password_hash($password, PASSWORD_BCRYPT));
-
-                if (! $userRepository->update($user)){
-                    $view['errors']['database'] = 'Error when updating new user';
-                    http_response_code(400);
-                }
-
-            } else{
-                http_response_code(400);
-            }
-
-            echo json_encode($view['errors']);
+        if (! $userRepository->update($user)){
+            $view['errors']['database'] = 'Error when updating new user';
+            http_response_code(400);
         }
+
+    } else{
+        http_response_code(400);
     }
+
+    echo json_encode($view['errors']);
 }
