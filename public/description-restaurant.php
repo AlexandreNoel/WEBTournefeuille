52166d8c5c114c887f3157bb6126f541a6f30a14
Yoan <rockyoan@gmail.com>
2018-10-28 04:52:11 +0100

    decription without comment & notation & badges & favorites

--- public/description-restaurant.php
+++ public/description-restaurant.php
@@ -4,13 +4,14 @@ require '../vendor/autoload.php';
 session_start();
 
 $restaurantRepository = new \Repository\Restaurant();
-
+$restaurantHydrator = new \Hydrator\Restaurant();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = $_POST['id_resto'];
 
     if ($id) {
         $error = $restaurantRepository->findOneById($id);
+        $error = $restaurantHydrator->extract($error);
     }else{
         http_response_code(400);
         $error = "error";
