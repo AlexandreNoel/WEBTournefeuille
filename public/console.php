58b5233cd3e8863a2464007e60aeefcafe4f7d38
Antoine CHAPUSOT <antoine.chapusot@ensiie.fr>
2018-10-28 20:00:35 +0100

    authentication OK

--- public/console.php
+++ public/console.php
@@ -12,10 +12,8 @@
         session_start();
 
     // Vérification si Admin connecté
-    $_SESSION['authenticated_admin']=true;
-    // Vérification si Admin connecté
     if(!isset($_SESSION['authenticated_admin'])){
-        header('Location: /');
+        header('Location: /connect-console.php');
     }
     else{
 
@@ -29,13 +27,14 @@
         $productRepository = new \Product\Repository\Product();
         $productHydrator = new \Product\Hydrator\Product();
 
-        /** @var \Client\Entity\Client $user */
-        $user =  $_SESSION["authenticated_user"];
-        // Récupèration des informations utilisateurs
-        $nickname = $user->getNickname();
-        $firstname = $user->getFirstName();
-        $lastname = $user->getLastName();
-        $solde = $user->getSolde();
+        // PAS D'UTILISATEUR CONNECTÉ MAIS UNE DEMANDE DU PASSWORD BARMAN À L'ACTION
+        // /** @var \Client\Entity\Client $user */
+        // $user =  $_SESSION["authenticated_user"];
+        // // Récupèration des informations utilisateurs
+        // $nickname = $user->getNickname();
+        // $firstname = $user->getFirstName();
+        // $lastname = $user->getLastName();
+        // $solde = $user->getSolde();
 
         //========================================
         // Traitements
