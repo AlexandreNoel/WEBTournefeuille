fbec3bcc918e812cd922b8c9d8c9a703d92736fa
xavier.grimaldi@live.fr <Vous@exemple.com>
2018-10-29 11:51:01 +0100

    Amélioration globale front

--- public/console.php
+++ public/console.php
@@ -21,15 +21,6 @@
         $productRepository = new \Product\Repository\Product();
         $productHydrator = new \Product\Hydrator\Product();
 
-        // PAS D'UTILISATEUR CONNECTÉ MAIS UNE DEMANDE DU PASSWORD BARMAN À L'ACTION
-        // /** @var \Client\Entity\Client $user */
-        // $user =  $_SESSION["authenticated_user"];
-        // // Récupèration des informations utilisateurs
-        // $nickname = $user->getNickname();
-        // $firstname = $user->getFirstName();
-        // $lastname = $user->getLastName();
-        // $solde = $user->getSolde();
-
         //========================================
         // Traitements
         //========================================
